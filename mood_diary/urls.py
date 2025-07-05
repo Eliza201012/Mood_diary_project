781bd940c7f2c93bb9081ef944ae38db9c2adfd8
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-05 23:04:33 +0300

    + delete/<int:id>/

--- mood_diary/urls.py
+++ mood_diary/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
-    path("edit/<int:id>/", views.edit, name="edit")
+    path("edit/<int:id>/", views.edit, name="edit"),
+    path("delete/<int:id>/", views.delete, name="delete")
 ]
