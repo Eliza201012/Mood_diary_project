ffefb77e6d9f85541933bc9363a51b465d527185
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-05 18:17:40 +0300

    + function edit

--- mood_diary/views.py
+++ mood_diary/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from .models import *
 
 def home(request):
@@ -16,3 +16,17 @@ def create(request):
         return HttpResponseRedirect("/")
     entries = Mood_diary.objects.order_by("-date")
     return render(request, "home.html", {"entries" : entries})
+
+def edit(request, id):
+    try:
+        mood_diary = Mood_diary.objects.get(id=id)
+        if request.method == "POST":
+            mood_diary.date = request.POST.get("date")
+            mood_diary.mood = request.POST.get("mood")
+            mood_diary.comment = request.POST.get("comment")
+            mood_diary.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"mood_diary" : mood_diary})
+    except Mood_diary.DoesNotExist:
+        return HttpResponseNotFound("<h2>Your entry was not found</h2>")
