03ae59b48035a018e2a4191b4191d108e09dd2b8
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-05 22:58:44 +0300

    + delete()

--- mood_diary/views.py
+++ mood_diary/views.py
@@ -30,3 +30,11 @@ def edit(request, id):
             return render(request, "edit.html", {"mood_diary" : mood_diary})
     except Mood_diary.DoesNotExist:
         return HttpResponseNotFound("<h2>Your entry was not found</h2>")
+    
+def delete(request, id):
+    try:
+        mood_diary = Mood_diary.objects.get(id=id)
+        mood_diary.delete()
+        return HttpResponseRedirect("/")
+    except Mood_diary.DoesNotExist:
+        return HttpResponseNotFound("<h2>Your entry was not found</h2>")
