6f3cab4ac4994a0ca71b2b3173ef572d0fc740ee
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-07-06 09:23:02 +0300

    + підтримка медіа файлів

--- mood_diary/urls.py
+++ mood_diary/urls.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -7,3 +10,7 @@ urlpatterns = [
     path("edit/<int:id>/", views.edit, name="edit"),
     path("delete/<int:id>/", views.delete, name="delete")
 ]
+
+# Додаємо підтримку медіа файлів у режимі розробки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
